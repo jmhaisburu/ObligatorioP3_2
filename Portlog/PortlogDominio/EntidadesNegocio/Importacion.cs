11792bc44bf87f52e62531ef735030803c53cdcf
jmhaisburu <javi.mh91@gmail.com>
2020-06-28 16:48:47 -0300

    cambios precio final

--- Portlog/PortlogDominio/EntidadesNegocio/Importacion.cs
+++ Portlog/PortlogDominio/EntidadesNegocio/Importacion.cs
@@ -21,7 +21,7 @@ namespace PortlogDominio.EntidadesNegocio
         public DateTime SalidaPrevista { get; set; }
         public Producto Producto { get; set; }
         public int Cantidad { get; set; }
-        public static decimal PrecioFinal { get; set; }
+        public decimal PrecioFinal { get; set; }
 
         public bool Validar()
         {
